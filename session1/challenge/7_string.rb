2570125d8fbe4bd500ea47ce783711455b909519
Nikita <latinisnotdead07@gmail.com>
2020-04-18 12:13:14 +0100

    Completed exercises for session1.

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,12 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  out = []
+  string.downcase.chars.each_with_index{|x,i|
+  out << string[i+1] if x=="r"}
+  out.join
 end
